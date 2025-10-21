1b827566eae52195556063279fdb27992096fdc7
D4rshan19 <dhimal.621185@swl.k12.oh.us>
2025-10-21 08:40:54 -0400

    day1

--- src/craps_sim.java
+++ src/craps_sim.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class craps_sim {
+    public static void main(String [] args) {
+        System.out.println("Welcome to the Craps Simulator!");
+        System.out.println(" Rule 1: If the sym is 2, 3, or 12 it is called 'crapping out' and the game is over with a loss.");
+        System.out.println(" Rule 2: If the sym is 7 or 11 it called a 'natural' and the game is over with a win. ");
+        System.out.println(" Rule 3: For all other values, the sum becomes 'the point' and the user makes subsequent rolls until \n they either roll a 7, in which case they loose, or they roll the point sum in which case they win. ");
+        Random rand = new Random();
+    }
 }
